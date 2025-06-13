38f01cbfd031d6051c3b3140d3a8792d53264480
Uxidone <zihmanisoskars05@gmail.com>
2025-06-13 15:10:10 +0300

    Update v2.js

--- src/routes/v2.js
+++ src/routes/v2.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
 const projectController = require('../controllers/projectController');
+const completionsController = require('../controllers/completionsController');
 
 // All routes require API key authentication
 router.use(auth);
@@ -9,5 +10,8 @@ router.use(auth);
 // Project endpoints
 router.get('/projects', projectController.getAllProjects);
 router.get('/projects/:id', projectController.getProjectById);
+
+// Completions endpoint
+router.post('/completions', completionsController.createCompletion);
 
 module.exports = router;
