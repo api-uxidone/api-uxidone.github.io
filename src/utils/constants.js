6812e07ef50a9afd0f8cf9e9eb2eb3ba7f92ea24
Uxidone <zihmanisoskars05@gmail.com>
2025-06-13 15:11:01 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 module.exports = {
   API_KEY_PREFIX: 'UXIDONE',
   API_VERSIONS: ['v1', 'v2'],
-  // Other constants...
+  COMPLETION_MODELS: {
+    DEFAULT: 'uxidone-model-v2',
+    PREMIUM: 'uxidone-model-premium-v2'
+  }
 };
